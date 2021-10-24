8d52772cfc3e6607844b72bffdca4e1634d8210a
mingkhoi <mingkhoitran1234@gmail.com>
2021-10-24 17:43:28 +0700

    container name (no swarm); support nginx, npm live-server

--- datanode-img/startup.sh
+++ datanode-img/startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-service ssh start && hdfs namenode -format -nonInteractive
+service ssh start && service nginx start && hdfs namenode -format -nonInteractive
 
 tail -f /container_share.log # keep container run
